f77c91014129a27784413a8815fb072aae15e833
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-31 14:23:17 +0300

    migrations for videos and some changes to user

---
+++ database/migrations/2018_03_31_101841_create_videos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid');
+            $table->integer('user_id')->unsigned();
+            $table->string('url')->nullable();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('visibility', ['public', 'private']);
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
