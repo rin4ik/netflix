f64cc1d741e8ce5af5c3162984a0fe2ff0e59ccc
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-31 21:32:38 +0300

    video player setted up

---
+++ resources/assets/js/components/VideoPlayer.vue
@@ -0,0 +1,58 @@
+<template>
+  <video
+   id="video"
+    class="video-js vjs-default-skin vjs-big-play-centered vjs-16-9"
+    controls preload="auto"
+     data-setup='{"fluid":true,"playbackRates": [0.5, 1, 1.5, 2,2.5] ]}'
+     :poster="thumbnailUrl"> 
+     <source :src="videoUrl" type='video/youtube'>
+  </video>
+
+</template>
+<script>
+import videojs from "video.js";
+import youtube from "videojs-youtube";
+import hotkeys from "videojs-hotkeys";
+export default {
+  props: {
+    videoUid: null,
+    videoUrl: null,
+    thumbnailUrl: null
+  },
+  data() {
+    return {
+      player: null,
+      duration: null
+    };
+  },
+  methods: {
+    hasHitQuotaView() {
+      if (!this.duration) {
+        return false;
+      }
+      return (
+        Math.round(this.player.currentTime()) ===
+        Math.round(10 * this.duration / 100)
+      );
+    },
+    createView() {
+      axios.post("/videos/" + this.videoUid + "/views");
+    }
+  },
+  mounted() {
+    this.player = videojs("video");
+    this.player.hotkeys({
+      seekStep: 10,
+      playbackRate: [0.5, 1, 1.5, 2, 2.5]
+    });
+    this.player.on("loadedmetadata", () => {
+      this.duration = Math.round(this.player.duration());
+    });
+    setInterval(() => {
+      if (this.hasHitQuotaView()) {
+        this.createView();
+      }
+    }, 1000);
+  }
+};
+</script>
