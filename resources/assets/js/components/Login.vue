7b9db051d078adec709d3a05385c30fe31fe4bf7
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-31 11:40:37 +0300

    auth laravel with vue modal

---
+++ resources/assets/js/components/Login.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      feedback: "",
+      form: {
+        email: "",
+        password: ""
+      },
+      loading: false
+    };
+  },
+  methods: {
+    login() {
+      this.loading = true;
+      axios
+        .post("/login", this.form)
+        .then(() => location.reload())
+        .catch(error => {
+          this.feedback = error.response.data.message;
+          this.loading = false;
+        });
+    },
+    register() {
+      this.$modal.hide("login");
+      this.$modal.show("register");
+    }
+  }
+};
+</script>
