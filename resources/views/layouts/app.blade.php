752f7a4d19a14f0a2f6437ab6bbaaaba1cd908c5
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-31 11:42:39 +0300

    wip

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+        <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script>
+        
+        window.App={!!json_encode(['csrfToken'=>csrf_token(),
+        'url'=>config('app.url'),
+        'user'=>Auth::user(),
+        'signedIn'=>Auth::check()
+        ])!!};
+    </script>
+    <style>
+     
+[v-cloak] {
+    display: none;
+  }
+    </style>
+</head>
+<body> 
+  
+    <div id="app">
+     @include('layouts.partials._nav')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+        <flash level="{{session('level')}}" message="{{session('flash')}}"></flash>
+        <div v-cloak >
+                @include('modals.all')
+               
+        </div> 
+        
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+    
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/js/mdb.min.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,64 @@
+<nav class="navbar navbar-expand-lg navbar-dark  blue-grey elegant-color" style="padding-top:3px;padding-bottom:3px;">
+    <div class="container">
+        <a class="navbar-brand " href="/">  Netflix
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           
+            <!-- Left Side Of Navbar -->
+            <form action="/search" method="get" class="form-inline">
+            <div class="form-group">
+                <input type="text" name="q" class="form-control " placeholder="Search" value="{{Request::get('q')}}">
+            </div>
+                <button class="btn btn-outline-white my-2 my-sm-0 btn-sm" type="submit" style="margin-left:10px">Search</button>
+            </form>
+
+            <ul class="navbar-nav mr-auto">
+               &nbsp;
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                <li>
+                <a class="nav-link"  @click="$modal.show('login')">{{ __('LOGIN') }}</a>
+                    {{--  //<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>  --}}
+                </li>
+                <li>
+                    <a class="nav-link"   @click="$modal.show('register')">{{ __('REGISTER') }}</a>
+                </li>
+                @else
+                <a class="nav-link" href="/upload">Upload</a>
+                
+                <li class="nav-item dropdown">
+
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                        <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{url('/videos')}}">Your videos</a>
+                        <a class="dropdown-item" href="{{url('/channel/'.$channel->slug)}}">Your channel</a>
+                        <a class="dropdown-item" href="{{url('/channel/'.$channel->slug.'/edit')}}">Channel settings</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
