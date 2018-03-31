4d7b3384f3fc5d79a732b4197745a7159e4c20d1
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-31 18:49:44 +0300

    User and video policy

---
+++ app/Policies/VideoPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Video;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VideoPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function edit(User $user, Video $video)
+    {
+        return $user->id === $video->user->id;
+    }
+}
